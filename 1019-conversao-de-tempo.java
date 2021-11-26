c2f02c2f1bdfd67f5aa87d67d092c225f87966b2
IsraelSantucci <34198562+IsraelSantucci@users.noreply.github.com>
2021-11-26 10:30:26 -0300

    Create 1019-conversao-de-tempo.java

---
+++ 1019-conversao-de-tempo.java
@@ -0,0 +1,43 @@
+import java.io.IOException;
+import java.util.Scanner;
+/**
+ * IMPORTANT: 
+ Leia um valor inteiro, que é o tempo de duração em segundos de um determinado evento em uma fábrica, e informe-o expresso no formato horas:minutos:segundos.
+
+Entrada
+O arquivo de entrada contém um valor inteiro N.
+
+Saída
+Imprima o tempo lido no arquivo de entrada (segundos), convertido para horas:minutos:segundos, conforme exemplo fornecido.
+
+Exemplo de Entrada	Exemplo de Saída
+556
+
+0:9:16
+
+1
+
+0:0:1
+
+140153
+
+38:55:53
+ */
+public class Main {
+ 
+    public static void main(String[] args) throws IOException {
+       Scanner scan = new Scanner(System.in);
+       Integer tempo = scan.nextInt();
+       
+       int hora = tempo / 3600;
+       
+       tempo = tempo%3600;
+       int min =tempo / 60;
+       
+       int sec = tempo % 60;
+       
+       System.out.println(hora+":"+min+":"+sec);
+ 
+    }
+ 
+}
